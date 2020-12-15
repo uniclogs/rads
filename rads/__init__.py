2af66b4ddfdc12db4de8875cdffa342a91f94d3b
ryanpdx <rmedick@pdx.edu>
2020-12-15 15:19:02 -0800

    simplied pip package build files

---
+++ rads/__init__.py
@@ -0,0 +1,11 @@
+MAJOR = 0
+MINOR = 1
+PATCH = 0
+
+APP_NAME = "rads"
+APP_DESCRIPTION = "A utility for approving/denying oresat live pass requests."
+APP_VERSION = "{}.{}.{}".format(MAJOR, MINOR, PATCH)
+APP_AUTHOR = "Ryan Medick"
+APP_EMAIL = "rmedick@pdx.edu"
+APP_URL = "https://github.com/oresat/uniclogs-rads"
+APP_LICENSE = "GPL-3.0"
--- setup.py
+++ setup.py
@@ -1,9 +1,51 @@
-# -*- coding: utf-8 -*-
-import os
-import sys
-
 import setuptools
+import rads
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
+with open('README.md', 'r') as file:
+    long_description = file.read()
 
-setuptools.setup()
+setuptools.setup(
+    name=rads.APP_NAME,
+    version=rads.APP_VERSION,
+    author=rads.APP_AUTHOR,
+    license=rads.APP_LICENSE,
+    description=rads.APP_DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=rads.APP_URL,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Environment :: Console :: Curses",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development :: User Interfaces",
+        "Topic :: System :: Monitoring",
+        "Topic :: System :: Networking :: Monitoring :: Hardware Watchdog"
+    ],
+    install_requires=[
+        "loguru",
+        "sqlalchemy",
+        "reverse_geocoder",
+        "pass-calculator",
+        "ballcosmos",
+    ],
+    extras_require={
+        "dev": [
+            "setuptools",
+            "wheel",
+            "pytest",
+            "flake8",
+            "twine",
+            "sphinx",
+            "sphinx_rtd_theme",
+        ]
+    },
+    python_requires='>=3.8.5',
+    entry_points={
+        "console_scripts": [
+            "rads = rads.ui.main_menu:main"
+        ]
+    }
+)
